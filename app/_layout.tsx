12fac6208be140c205f2874969ba5f37251c4305
ESIANNAN <pannan027@st.edu.gh>
2026-08-19 00:54:36 +0000

    feat: initialize root layout with navigation stack, theme provider, and platform-specific system UI configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout() {
           <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
 
           <Stack.Screen
-            name='sheet'
+            name='(settings)/sheet'
             options={{
               headerShown: false,
               sheetGrabberVisible: true,
